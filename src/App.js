ce5a047f8a4efa094dec91f05e5861c59e3c50c4
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-02-10 17:46:17 +0530

    application

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className='App'>REact test app</div>;
+  return <div className='App'>REact test application</div>;
 }
 
 export default App;
